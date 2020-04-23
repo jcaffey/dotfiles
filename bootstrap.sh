ea5b752250bfc9adb291c9d7a2397c37ec942a3d
Josh Caffey <jcaffey@gmail.com>
2020-04-23 00:27:37 -0500

    install dependencies for dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,62 @@
-VAGRANT_HOME="/home/vagrant"
 sudo apt-get update
-sudo apt-get install --yes curl git silversearcher-ag stow vim-gtk3 zplug zsh
+sudo apt-get install --yes build-essential cmake curl wget git gnupg2 python3-dev silversearcher-ag stow vim-gtk3 zsh
+
+VAGRANT_HOME="/home/vagrant"
+DOTFILES=$VAGRANT_HOME/dotfiles
+
+cd $VAGRANT_HOME
+
+# rvm
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source $VAGRANT_HOME/.rvm/scripts/rvm
+rvm install 2.6.3
+rvm --default use 2.6.3
+
+# nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$VAGRANT_HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install 12.14.0
+nvm use 12
+npm install -g yarn
+
+# ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-zsh
-chsh -s $(which zsh) vagrant
-git clone -b vagrant https://github.com/jcaffey/dotfiles $VAGRANT_HOME/dotfiles
-git clone https://github.com/zplug/zplug $VAGRANT_HOME/.zplug
-DOTFILES_HOME=$VAGRANT_HOME
-cd $VAGRANT_HOME/dotfiles
+
+# zplug
+export ZPLUG_HOME=$VAGRANT_HOME/.zplug
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
+
+# set default shell
+sudo chsh -s $(which zsh) vagrant
+
+# spaceship prompt
+export ZSH_CUSTOM=$VAGRANT_HOME/.oh-my-zsh/custom
+mkdir -p $ZSH_CUSTOM/themes/spaceship-prompt
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# dotfiles
+git clone https://github.com/jcaffey/dotfiles.git $DOTFILES
+cd $DOTFILES
+rm ~/.nvm/.npmrc ~/.zshrc
 stow -v ack bin git nvm tmux vim zsh
-# TODO: utilities like fzf, zplug addons, etc..
-#NVM_PATH=$VAGRANT_HOME/.nvm
-#mkdir -p $NVM_PATH
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | NVM_DIR=$NVM_PATH zsh
-##source $VAGRANT_HOME/.zshrc
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install 
+
+# better diffs with diff-so-fancy
+yarn global add diff-so-fancy
+
+# vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install plugins
+# vim +PluginInstall +qall
+#
+# YCM
+# cd ~/.vim/bundle/YouCompleteMe
+# python3 install.py --all
