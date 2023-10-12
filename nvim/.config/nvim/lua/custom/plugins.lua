9de6785153c20c325b2cbda70522363c936aa377
Josh Caffey <jcaffey@gmail.com>
2023-10-11 20:42:42 -0500

    fix rvm in fish

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -113,6 +113,19 @@ local plugins = {
    lazy = false,
    config = function() require("nnn").setup() end
   },
+
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  }
+
+
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
