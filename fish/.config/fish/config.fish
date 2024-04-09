37eab02720fc600a0342837fa2baf2960ee2956f
Josh Caffey <jcaffey@gmail.com>
2024-04-09 15:57:11 -0500

    add go path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,6 +14,7 @@ fish_add_path ~/.rvm/bin
 fish_add_path /opt/homebrew/opt/go@1.20/bin
 fish_add_path ~/.ebcli-virtual-env/executables # elastic beanstalk cli
 fish_add_path ~/jcaffey/.local/share/nvim/mason/bin # mason install path for nvim tools
+fish_add_path ~/go/bin # binaries from go install
 
 # environment variables
 set -gx RIPGREP_CONFIG_PATH $HOME/ripgrep.rc
