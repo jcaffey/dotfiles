459c3e1ea5ad25216962f27c26328828b45704b5
Josh Caffey <jcaffey@gmail.com>
2024-09-01 21:03:56 -0500

    wezterm doesnt use xdg home

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,81 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- CONFIG CUSTOMIZATION STARTS HERE
+-- ================================
+
+-- Spawn a fish shell in login mode
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'catppuccin-macchiato' -- good for default text and im just about that catppuccin life
+-- config.color_scheme = 'Catppuccin Macchiato' -- good for default text and im just about that catppuccin life
+-- config.color_scheme = 'Ciapre' -- easier to read default (white) text
+-- config.color_scheme = 'Dracula'
+-- config.color_scheme = 'duckbones'
+-- config.color_scheme = 'duskfox' -- very good!
+
+config.colors = {
+  cursor_bg = "#236D5F",
+  cursor_fg = "#000",
+}
+
+-- transparency
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 60
+config.text_background_opacity = 0.3
+
+-- font
+config.font = wezterm.font 'JetBrains Mono'
+-- config.font = wezterm.font 'MonoLisa'
+config.font_size = 20
+
+-- window options
+config.window_decorations = "RESIZE"
+config.native_macos_fullscreen_mode = true
+config.tab_bar_at_bottom = true
+config.enable_tab_bar = true
+config.enable_scroll_bar = false
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+-- scrollback history per tab
+config.scrollback_lines = 3500
+
+-- and finally, return the configuration to wezterm
+return config
+
+-- DEFAULT CONFIG FOR DEBUGGING
+-- ============================
+-- -- Pull in the wezterm API
+-- local wezterm = require 'wezterm'
+--
+-- -- This table will hold the configuration.
+-- local config = {}
+--
+-- -- In newer versions of wezterm, use the config_builder which will
+-- -- help provide clearer error messages
+-- if wezterm.config_builder then
+--   config = wezterm.config_builder()
+-- end
+--
+-- return config
