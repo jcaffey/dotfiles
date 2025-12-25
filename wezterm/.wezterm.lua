a960a3b086d0f2e29f4e9013926610f06928d5ce
Josh Caffey <jcaffey@gmail.com>
2025-12-25 02:08:45 -0600

    rice it up

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -51,7 +51,7 @@ config.colors = {
 }
 
 -- transparency
-config.window_background_opacity = 0.84
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 60
 config.text_background_opacity = 0.3
 
